d47babc2f9133946a25e4bb1a94aa0e2b72c66bf
Guilherme Gomes <77558872+guilhermerrg15@users.noreply.github.com>
2024-12-02 16:46:35 -0300

    Update codigo_tela.ino

--- codigo_tela.ino
+++ codigo_tela.ino
@@ -2,6 +2,20 @@
 #include <MCUFRIEND_kbv.h>
 #include <TouchScreen.h>
 #include <JKSButton.h>
+#include <SPI.h>
+#include <SdFat.h> 
+#include <MFRC522.h>
+
+#define RST_PIN 22
+#define SS_PIN 53
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+byte NEW_UID[] = { 0x3b, 0xaa, 0xaa, 0xaa };
+
+String rfidHex;
+
+MFRC522::MIFARE_Key key;
+String content = "";
 
 MCUFRIEND_kbv tela;
 TouchScreen touch(6, A1, A2, 7, 300);
@@ -10,6 +24,31 @@ botao_Grava_Infra, botao_Emite_Infra, botaoRFID_Lista_ex, botaoInfra_Lista_ex,
 botao_Proximo_2_1_2, botao_Proximo_2_2, botao_Encerra_2_1_3, botao_Renomeia_ID,
 botao_Termina_3_1_2, botao_Proximo_3_2, botao_Proximo_3_2_2, botao_Sim_3_2_3,
 botao_Nao_3_2_3, botao_Volta_Tela_1;
+
+SdFatSoftSpi<12, 11, 13> SD;
+
+#define SD_CS 10
+#define NAMEMATCH ""
+#define PALETTEDEPTH 0
+#define BMPIMAGEOFFSET 54
+#define BUFFPIXEL 20
+
+char namebuf[32] = "/";
+
+File root;
+int pathlen;
+
+uint16_t read16(File& f) {
+  uint16_t result;
+  f.read(&result, sizeof(result));
+  return result;
+}
+
+uint32_t read32(File& f) {
+  uint32_t result;
+  f.read(&result, sizeof(result));
+  return result;
+}
 
 enum {
   Tela_1, Tela_2, Tela_2_1,
@@ -21,13 +60,30 @@ enum {
 int idx_tela = Tela_1;
 
 void setup() {
+  Serial.begin(9600);
   tela.begin( tela.readID() );
   tela_1();
 
+  SPI.begin();
+  mfrc522.PCD_Init();
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  
+  bool good = SD.begin(SD_CS);
+  if (!good) {
+   Serial.print("cannot start SD");
+    //while(1);
+  }
+  root = SD.open(namebuf);
+  pathlen = strlen(namebuf);
 }
 
 void loop() {
+  
+
   if (idx_tela == Tela_1) {
+   // Serial.println(showBMP("logo3.bmp", 5, 5));
     botao_RFID.process();
     botao_Infra.process();
   }
@@ -42,7 +98,18 @@ void loop() {
   if(idx_tela == Tela_2_1_3) {
     botao_Encerra_2_1_3.process();
   }
-  if(idx_tela == Tela_2_2) {
+  if(idx_tela == Tela_2_2 && mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    //Serial.print(F("Card UID:"));
+    rfidHex = "";
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      rfidHex += mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ";
+      rfidHex += String(mfrc522.uid.uidByte[i], HEX);
+      NEW_UID[i] = mfrc522.uid.uidByte[i];
+    }
+
+    idx_tela = Tela_2_2_2;
+    tela_2_2_2();
     botao_Proximo_2_2.process();
   }
   if (idx_tela == Tela_2_2_2) {
@@ -180,7 +247,9 @@ void tela_2_2_2() {
   tela.setCursor(30, 150);
   tela.setTextColor(TFT_WHITE);
   tela.setTextSize(2);
-  tela.print("ID x adicionado\n    com sucesso.");
+  tela.println("ID " + rfidHex + " adicionado\n    com sucesso.");
+  tela.setCursor(60, 250);
+
   botao_Renomeia_ID.init(&tela, &touch, 180, 300, 120, 30, TFT_BLACK, TFT_RED,
 TFT_WHITE, "Renomear", 2);
   botao_Renomeia_ID.setPressHandler(goto_tela_1);
@@ -281,4 +350,146 @@ TFT_WHITE, "RFID", 2);
 TFT_WHITE, "Infravermelho", 1);
   botao_RFID.setPressHandler(goto_tela_2);
   botao_Infra.setPressHandler(goto_tela_3);
+  
 }
+
+uint8_t showBMP(char *nm, int x, int y)
+{
+    File bmpFile;
+    int bmpWidth, bmpHeight;    // W+H in pixels
+    uint8_t bmpDepth;           // Bit depth (currently must be 24, 16, 8, 4, 1)
+    uint32_t bmpImageoffset;    // Start of image data in file
+    uint32_t rowSize;           // Not always = bmpWidth; may have padding
+    uint8_t sdbuffer[3 * BUFFPIXEL];    // pixel in buffer (R+G+B per pixel)
+    uint16_t lcdbuffer[(1 << PALETTEDEPTH) + BUFFPIXEL], *palette = NULL;
+    uint8_t bitmask, bitshift;
+    boolean flip = true;        // BMP is stored bottom-to-top
+    int w, h, row, col, lcdbufsiz = (1 << PALETTEDEPTH) + BUFFPIXEL, buffidx;
+    uint32_t pos;               // seek position
+    boolean is565 = false;      //
+
+    uint16_t bmpID;
+    uint16_t n;                 // blocks read
+    uint8_t ret;
+
+    if ((x >= tela.width()) || (y >= tela.height()))
+        return 1;               // off screen
+
+    bmpFile = SD.open(nm);      // Parse BMP header
+    bmpID = read16(bmpFile);    // BMP signature
+    (void) read32(bmpFile);     // Read & ignore file size
+    (void) read32(bmpFile);     // Read & ignore creator bytes
+    bmpImageoffset = read32(bmpFile);       // Start of image data
+    (void) read32(bmpFile);     // Read & ignore DIB header size
+    bmpWidth = read32(bmpFile);
+    bmpHeight = read32(bmpFile);
+    n = read16(bmpFile);        // # planes -- must be '1'
+    bmpDepth = read16(bmpFile); // bits per pixel
+    pos = read32(bmpFile);      // format
+    if (bmpID != 0x4D42) ret = 2; // bad ID
+    else if (n != 1) ret = 3;   // too many planes
+    else if (pos != 0 && pos != 3) ret = 4; // format: 0 = uncompressed, 3 = 565
+    else if (bmpDepth < 16 && bmpDepth > PALETTEDEPTH) ret = 5; // palette 
+    else {
+        bool first = true;
+        is565 = (pos == 3);               // ?already in 16-bit format
+        // BMP rows are padded (if needed) to 4-byte boundary
+        rowSize = (bmpWidth * bmpDepth / 8 + 3) & ~3;
+        if (bmpHeight < 0) {              // If negative, image is in top-down order.
+            bmpHeight = -bmpHeight;
+            flip = false;
+        }
+
+        w = bmpWidth;
+        h = bmpHeight;
+        if ((x + w) >= tela.width())       // Crop area to be loaded
+            w = tela.width() - x;
+        if ((y + h) >= tela.height())      //
+            h = tela.height() - y;
+
+        if (bmpDepth <= PALETTEDEPTH) {   // these modes have separate palette
+            bmpFile.seek(BMPIMAGEOFFSET); //palette is always @ 54
+            bitmask = 0xFF;
+            if (bmpDepth < 8)
+                bitmask >>= bmpDepth;
+            bitshift = 8 - bmpDepth;
+            n = 1 << bmpDepth;
+            lcdbufsiz -= n;
+            palette = lcdbuffer + lcdbufsiz;
+            for (col = 0; col < n; col++) {
+                pos = read32(bmpFile);    //map palette to 5-6-5
+                palette[col] = ((pos & 0x0000F8) >> 3) | ((pos & 0x00FC00) >> 5) | ((pos & 0xF80000) >> 8);
+            }
+        }
+
+        // Set TFT address window to clipped image bounds
+        tela.setAddrWindow(x, y, x + w - 1, y + h - 1);
+        for (row = 0; row < h; row++) { // For each scanline...
+            // Seek to start of scan line.  It might seem labor-
+            // intensive to be doing this on every line, but this
+            // method covers a lot of gritty details like cropping
+            // and scanline padding.  Also, the seek only takes
+            // place if the file position actually needs to change
+            // (avoids a lot of cluster math in SD library).
+            uint8_t r, g, b, *sdptr;
+            int lcdidx, lcdleft;
+            if (flip)   // Bitmap is stored bottom-to-top order (normal BMP)
+                pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+            else        // Bitmap is stored top-to-bottom
+                pos = bmpImageoffset + row * rowSize;
+            if (bmpFile.position() != pos) { // Need seek?
+                bmpFile.seek(pos);
+                buffidx = sizeof(sdbuffer); // Force buffer reload
+            }
+
+            for (col = 0; col < w; ) {  //pixels in row
+                lcdleft = w - col;
+                if (lcdleft > lcdbufsiz) lcdleft = lcdbufsiz;
+                for (lcdidx = 0; lcdidx < lcdleft; lcdidx++) { // buffer at a time
+                    uint16_t color;
+                    // Time to read more pixel data?
+                    if (buffidx >= sizeof(sdbuffer)) { // Indeed
+                        bmpFile.read(sdbuffer, sizeof(sdbuffer));
+                        buffidx = 0; // Set index to beginning
+                        r = 0;
+                    }
+                    switch (bmpDepth) {          // Convert pixel from BMP to TFT format
+                        case 24:
+                            b = sdbuffer[buffidx++];
+                            g = sdbuffer[buffidx++];
+                            r = sdbuffer[buffidx++];
+                            color = tela.color565(r, g, b);
+                            break;
+                        case 16:
+                            b = sdbuffer[buffidx++];
+                            r = sdbuffer[buffidx++];
+                            if (is565)
+                                color = (r << 8) | (b);
+                            else
+                                color = (r << 9) | ((b & 0xE0) << 1) | (b & 0x1F);
+                            break;
+                        case 1:
+                        case 4:
+                        case 8:
+                            if (r == 0)
+                                b = sdbuffer[buffidx++], r = 8;
+                            color = palette[(b >> bitshift) & bitmask];
+                            r -= bmpDepth;
+                            b <<= bmpDepth;
+                            break;
+                    }
+                    lcdbuffer[lcdidx] = color;
+
+                }
+                tela.pushColors(lcdbuffer, lcdidx, first);
+                first = false;
+                col += lcdidx;
+            }           // end cols
+        }               // end rows
+        tela.setAddrWindow(0, 0, tela.width() - 1, tela.height() - 1); //restore full screen
+        ret = 0;        // good render
+    }
+    bmpFile.close();
+    return (ret);
+}
+
